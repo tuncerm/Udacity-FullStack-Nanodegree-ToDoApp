587d61af7b190a38fd7cc69aeaec7d77c8e400a0
Mustafa TUNCER <tuncerm@gmail.com>
2020-04-04 21:51:56 +0200

    Basic todo with template and data.

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html', data=[{
+        'description': 'Todo 1'
+    }, {
+        'description': 'Todo 2'
+    }, {
+        'description': 'Todo 3'
+    }])
 
 
 if __name__ == '__main__':
